07c7bd393edf4672a3d9014be36f3403a982fc83
DaViL <stalker.gluhoy@gmail.com>
2022-09-03 23:14:13 +0700

    All scripts are working

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -7,6 +7,8 @@ public class WanderingAI : MonoBehaviour
     public float speed = 3.0f;
     public float obtacleRange = 5.0f;
     private bool _alive;
+    private GameObject _fireball;
+    [SerializeField] private GameObject fireballPrefab;
 
     void Start()
     {
@@ -23,7 +25,17 @@ public class WanderingAI : MonoBehaviour
             RaycastHit hit;
             if (Physics.SphereCast(ray, 0.75f, out hit))
             {
-                if (hit.distance < obtacleRange)
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>())
+                {
+                    if(_fireball == null)
+                    {
+                        _fireball = Instantiate(fireballPrefab) as GameObject;
+                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                        _fireball.transform.rotation = transform.rotation;
+                    }
+                }
+                else if (hit.distance < obtacleRange)
                 {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
